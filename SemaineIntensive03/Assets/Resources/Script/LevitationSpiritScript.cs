95c4a782cf70779e589cf65d2e38ffae83e9406c
Miformat <m.humeau@J001-0136-12.etudiant.rubika-edu.com.local>
2016-03-10 18:39:11 +0100

    ghost head + new UI 50%

--- SemaineIntensive03/Assets/Resources/Script/LevitationSpiritScript.cs
+++ SemaineIntensive03/Assets/Resources/Script/LevitationSpiritScript.cs
@@ -15,31 +15,35 @@ public class LevitationSpiritScript : MonoBehaviour
 	public float deathDuration = 1.5f;
 	public Ease easeTypeDeath = Ease.InOutQuad;
 
+	private float yPosition;
+
 	// Use this for initialization
 	void Start () 
 	{
 		DOTween.Init ();
+
+		yPosition = transform.localPosition.y;
 	}
 
 	public void Death ()
 	{
 		DOTween.Pause ("Levitation");
-		transform.DOLocalMoveY (transform.position.y + deathLevitationHeight, deathDuration).SetEase (easeTypeDeath);
+		transform.DOLocalMoveY (yPosition + deathLevitationHeight, deathDuration).SetEase (easeTypeDeath);
 	}
 
 	public void LevitationVoid ()
 	{
-		DOTween.Pause ("Levitation");
+		//DOTween.Pause ("Levitation");
 		StartCoroutine (Levitation ());
 	}
 
 	IEnumerator Levitation ()
 	{
-		Tween mytween = transform.DOLocalMoveY (transform.position.y + levitationHeight, upSpeed).SetEase(easeType).SetId("Levitation");
+		Tween mytween = transform.DOLocalMoveY (yPosition + levitationHeight, upSpeed).SetEase(easeType).SetId("Levitation");
 
 		yield return mytween.WaitForCompletion ();
 
-		mytween = transform.DOLocalMoveY (transform.position.y - levitationHeight, downSpeed).SetEase(easeType).SetId("Levitation");
+		mytween = transform.DOLocalMoveY (yPosition - levitationHeight, downSpeed).SetEase(easeType).SetId("Levitation");
 
 		yield return mytween.WaitForCompletion ();
 	}
--- SemaineIntensive03/Assets/Resources/Script/lifewings.cs
+++ SemaineIntensive03/Assets/Resources/Script/lifewings.cs
@@ -9,6 +9,9 @@ public class lifewings : MonoBehaviour {
 
 	public List<UnityEngine.UI.Image> plumesList;
 
+	public List<Animator> animationlifeList;
+
+
 	public UnityEngine.Sprite lifeH;
 	public UnityEngine.Sprite deadH;
 
@@ -16,50 +19,125 @@ public class lifewings : MonoBehaviour {
 	public UnityEngine.Sprite deadB;
 
 
+	public Animator anim; 
+	 
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	 
+		P =  GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerScript> ();
+
+
+		foreach (Animator e in animationlifeList) {
+			e.gameObject.SetActive(false);
+		}
+	} 
+
+	private int oldvalue = -1;
+
+	public int decal = 0;
+
+
 	// Update is called once per frame
 	void Update () {
-		P =  GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerScript> ();
-		if (P) 
-		{
-			float life = P.life;
-			float lifeMax = P.maxLife;
-			
-			if (life <= 0f)
-				life = 0f;
-			int nombreDePlume = (int)((life/lifeMax)*8f);
-			
-			
-			if (life <= 0f)
-				life = 0f;
-			
-			
+		float life = P.life;
+		float lifeMax = P.maxLife;
+
+		if(life <= 0f)
+			life = 0f;
+		
+		if( life == lifeMax && decal != 0){ 
+
+			decal = 0;
+
 			for (int i = 0; i < 8 ; i++) {
-				if ( i  < nombreDePlume ) {
-					if (i % 2 == 0) {
-						plumesList[i].sprite = lifeH;
-					}
-					
-					else {
-						plumesList[i].sprite = lifeB;
-					}
+				
+				if (i % 2 == 0) {
+					plumesList[i].sprite = lifeH;
 				}
+
 				else {
-					if (i % 2 == 0) {
-						plumesList[i].sprite = deadH;
-					}
-					
-					else {
-						plumesList[i].sprite = deadB;
-					}
+					plumesList[i].sprite = lifeB;
 				}
-			}
+
+			} 
+		} 
+
+
+
+
+		int nombreDePlume = (int)((life/lifeMax)*8f);
+
+
+		if (life == 0 & decal == 0) {
+			nombreDePlume = 0;
 		}
 
+
+		if(life <= 0f)
+			life = 0f;
+		
+
+		if( nombreDePlume == 0f) {
+			plumesList[0].sprite = deadH;
+		}
+
+		if (nombreDePlume == 8)
+			nombreDePlume = 7;
+
+
+
+		if (oldvalue == -1) {
+			oldvalue = nombreDePlume;
+		}
+
+		else if(oldvalue > nombreDePlume) {
+			int lostPlume = oldvalue - nombreDePlume;
+
+			for(int i = 0 ; i < lostPlume ; i++) {
+				
+				animationlifeList[7 - (decal + i) ].gameObject.SetActive(true);
+				if ( (7-decal+i) % 2 == 0) {
+					StartCoroutine( deactivate(animationlifeList[7 - (decal + i) ].gameObject, plumesList[7 - (decal + i) ] , deadH ,animationlifeList[7 - (decal + i) ].GetCurrentAnimatorClipInfo(0)[0].clip.length *0.5f) );
+				}
+
+				else {
+					StartCoroutine( deactivate(animationlifeList[7 - (decal + i) ].gameObject, plumesList[7 - (decal + i) ] , deadB ,animationlifeList[7 - (decal + i) ].GetCurrentAnimatorClipInfo(0)[0].clip.length *0.5f) );
+				}
+			} 
+
+			oldvalue = nombreDePlume;
+
+			decal += lostPlume;
+		}
+
+		else {
+			oldvalue = nombreDePlume;
+		}
 	}
+
+	IEnumerator deactivate(GameObject Ob, UnityEngine.UI.Image plume , UnityEngine.Sprite e  ,float time1) {
+		plume.gameObject.SetActive(false);
+		yield return new WaitForSeconds(time1);
+		Ob.SetActive(false);
+		plume.sprite = e; 
+		plume.gameObject.SetActive(true);
+	}
+
+
+	public void reset() {
+		decal = 0; 
+
+		for (int i = 0; i < 8 ; i++) {
+
+			if (i % 2 == 0) {
+				plumesList[i].sprite = lifeH;
+			}
+
+			else {
+				plumesList[i].sprite = lifeB;
+			}
+
+		} 
+	}
+
 }
