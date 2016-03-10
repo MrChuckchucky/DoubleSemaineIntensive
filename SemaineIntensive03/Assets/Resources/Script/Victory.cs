7204f9509d2aa88cd037413cc2e424812e38827e
Miformat <m.humeau@J001-0136-12.etudiant.rubika-edu.com.local>
2016-03-11 00:51:00 +0100

    add victory screen

---
+++ SemaineIntensive03/Assets/Resources/Script/Victory.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Victory : MonoBehaviour
+{
+    public float maxSize;
+    public float growFactor;
+    public float waitTime;
+
+    public GameObject VictoryUI;
+
+    void Start()
+    {
+        StartCoroutine(Scale());
+        VictoryUI.SetActive(true);
+		StartCoroutine(WaitAnim());
+    }
+
+    IEnumerator Scale()
+    {
+        float timer = 0;
+
+        while (true)
+        {
+            while (maxSize > transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(waitTime);
+
+            timer = 0;
+            while (1 < transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+
+            timer = 0;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+	IEnumerator WaitAnim() {
+		yield return new WaitForSeconds (8);
+		Application.LoadLevel(0);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Joystick1Button7)) {
+			// Retour au menu
+			Application.LoadLevel(0);
+		}
+	}
+}
